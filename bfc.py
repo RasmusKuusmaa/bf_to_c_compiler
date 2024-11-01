af41f5cac733793896a0e15b01707cf382b492ea
77f315 <77f315@gmail.com>
2024-11-01 19:31:58 +0200

    moving pointer, adding vals, printing

--- bfc.py
+++ bfc.py
@@ -1,14 +1,27 @@
 def bf_to_c(bf_code, out='output.c'):
-    c_code = """
+    c_code = """#include <stdio.h>
 int main() {
     char memory[30000] = {0};
-    char *ptr = memory;
+    char *ptr = memory;\n
+"""
 
-    """
+    for cmd in bf_code:
+        if cmd == '>':
+            c_code += "\t++ptr;\n"
+        elif cmd == '<':
+            c_code += "\t--ptr;\n"
+        elif cmd == '+':
+            c_code += "\t++(*ptr);\n"
+        elif cmd == '-':
+            c_code += "\t--(*ptr);\n"
+        elif cmd == '.':
+            c_code += "\tputchar(*ptr);\n"
+
     c_code += """
     return 0;
 }
 """
     with open(out, 'w') as f:
         f.write(c_code)
-bf_to_c('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.')
+
+bf_to_c('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.<.>.')
